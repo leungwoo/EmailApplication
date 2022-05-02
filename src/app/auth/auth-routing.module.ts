42a304495f36bb41ecdd31193bfe894753e0b446
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-05-02 17:39:02 -0400

    Sign up form set up

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -4,8 +4,8 @@ import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  { path: '', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
+  { path: '', component: SigninComponent },
 ];
 
 @NgModule({
