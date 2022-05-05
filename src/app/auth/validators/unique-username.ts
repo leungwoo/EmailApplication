97538ca4aad73f3967f8c3934143caa675f9fb8f
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-05-05 13:13:14 -0400

    Service created

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,35 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { AbstractControl, AsyncValidator, FormControl } from '@angular/forms';
 import { map, catchError, of } from 'rxjs';
+import { AuthService } from '../auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class UniqueUsername implements AsyncValidator {
-  constructor(private http: HttpClient) {}
+  constructor(private authService: AuthService) {}
 
   validate = (control: AbstractControl) => {
     const { value } = control;
 
-    return this.http
-      .post<any>('https://api.angular-email.com/auth/username', {
-        username: value,
+    return this.authService.usernameAvailbale(value).pipe(
+      map((value) => {
+        if (value.available) {
+          console.log(value);
+          return control.value;
+        }
+      }),
+      catchError((err) => {
+        console.log(err);
+        if (err.error.username) {
+          return of({ nonUniqueUsername: true });
+        } else {
+          return of({ noConnection: true });
+        }
       })
-      .pipe(
-        map((value) => {
-          if (value.available) {
-            console.log(value);
-            return control.value;
-          }
-        }),
-        catchError((err) => {
-          console.log(err);
-          if (err.error.username) {
-            return of({ nonUniqueUsername: true });
-          } else {
-            return of({ noConnection: true });
-          }
-        })
-      );
+    );
   };
 }
 
