c5915806c3e2272755fdbbc8bc853fdce9384940
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-05-10 13:47:44 -0400

    Async pipe setup optional to use or just stick with subscribe method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,17 @@ import { AuthService } from './auth/auth.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  signedin: boolean = false;
-  constructor(private authService: AuthService) {}
+  // signedin: boolean = false;
+  signedin$: BehaviorSubject<boolean>;
 
-  ngOnInit() {
-    this.authService.signedin$.subscribe((signedin) => {
-      this.signedin = signedin;
-    });
+  constructor(private authService: AuthService) {
+    this.signedin$ = this.authService.signedin$;
   }
+
+  // ngOnInit() {
+  //   this.authService.signedin$.subscribe((signedin) => {
+  //     this.signedin = signedin;
+  //   });
+  // }
 }
+//Either use the subscribe method or use the Async pipe to get the data from the property that updates the view
