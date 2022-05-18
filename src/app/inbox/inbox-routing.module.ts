2cefdcc9b736fc21ee0c245afa785c01329f6719
Richard Leung Woo-Gabriel <RLWG@Richards-MacBook-Pro.local>
2022-05-17 21:45:59 -0400

    Child routing setup

--- src/app/inbox/inbox-routing.module.ts
+++ src/app/inbox/inbox-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../inbox/home/home.component';
+import { EmailShowComponent } from './email-show/email-show.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent, children:[
+  {path:':id',component: EmailShowComponent},
+  {path:'', component: PlaceholderComponent}] }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
