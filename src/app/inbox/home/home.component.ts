6dc97c7a74823e70b9ab5737ca65e62b7452adb8
Richard Leung Woo-Gabriel <RLWG@Richards-MacBook-Pro.local>
2022-05-19 23:52:03 -0400

    Added EmailForm component to embedded in the modal

--- src/app/inbox/home/home.component.ts
+++ src/app/inbox/home/home.component.ts
@@ -6,13 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-openModal = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-onClick(){
-  this.openModal = !this.openModal;
-}
+
 }
 
