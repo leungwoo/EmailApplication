a798ab3aedcf2379a4de03434176ddede1750ac4
Richard Leung Woo-Gabriel <RLWG@Richards-MacBook-Pro.local>
2022-05-19 22:18:09 -0400

    modal will be embedded in emailcreate component

--- src/app/inbox/email-create/email-create.component.ts
+++ src/app/inbox/email-create/email-create.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email-create.component.css']
 })
 export class EmailCreateComponent implements OnInit {
-
+  showModal = false;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+ 
 }
